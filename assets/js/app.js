1c553adb020b19a69bdbc18a68c70b6626d91dfd
Katlin <katjohnson926@yahoo.com>
2018-10-22 15:10:24 -0400

    made changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,46 @@
-//showing the index page
-const show btn1 = function(){
-    $(`.btn1`).hide();
-    $('.html').show();
+(function(){
+    $(`.form1`).hide;
+    $(`.form2`).hide;
+    $(`.form3`).hide;
+    $(`.form4`).hide;
+})();
+
+const showIndex = function(){
+    $(`h1`).hide;
+    $(`.form1`).hide;
+    $(`.form2`).hide;
+    $(`.form3`).hide;
+    $(`.form4`).hide;
+}
+
+const showAdd = function(){
+    $(`h1`).hide;
+    $(`.form1`).show;
+    $(`.form2`).hide;
+    $(`.form3`).hide;
+    $(`.form4`).hide;
+}
+
+const showVerify = function(){
+    $(`h1`).hide;
+    $(`.form1`).hide;
+    $(`.form2`).show;
+    $(`.form3`).hide;
+    $(`.form4`).hide;
+}
+
+const showUpdate = function(){
+    $(`h1`).hide;
+    $(`.form1`).hide;
+    $(`.form2`).hide;
+    $(`.form3`).show;
+    $(`.form4`).hide;
+}
+
+const showDelete = function(){
+    $(`h1`).hide;
+    $(`.form1`).hide;
+    $(`.form2`).hide;
+    $(`.form3`).hide;
+    $(`.form4`).show;
 }
